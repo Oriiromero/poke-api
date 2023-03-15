54844abb2dfd3b3466926a3e801e903f09f7cfd7
Oriiromero <oriana.nahir.r@gmail.com>
2023-03-15 22:51:28 +0100

    to do list done

---
+++ list.js
@@ -0,0 +1,47 @@
+let input$$ = document.querySelector('input');
+let btn$$ = document.querySelector('button');
+let ul$$ = document.querySelector('ul');
+let text$$ = document.querySelector('.empty');
+
+let initText = text$$.textContent;
+
+
+let numTasks = 0;
+
+btn$$.addEventListener('click', function(e) {
+    let str = input$$.value;
+
+    if(str.replace(/\s/g, '').length == 0){
+        return;
+    }
+
+    numTasks++;
+
+    text$$.textContent = '';
+
+
+    let li$$ = document.createElement('li');
+    e.preventDefault();
+
+    let eraseBtn = document.createElement('button');
+    eraseBtn.classList.add('erase');
+    eraseBtn.textContent = 'X';
+
+    
+    li$$.textContent = input$$.value;
+    li$$.appendChild(eraseBtn);
+    
+    eraseBtn.addEventListener('click', function() {
+        numTasks--;
+        
+        if(numTasks <= 0){
+            text$$.textContent = initText;
+        }
+
+        li$$.remove();
+    })
+
+    ul$$.appendChild(li$$);
+    
+   
+});
